f1b79fe7ad8cdf171ed1552c5e039252eca764b7
Albert Andrejev <albert.andrejev@gmail.com>
2020-11-24 18:26:24 +0200

    Fix module exports

--- src/Transactions/index.ts
+++ src/Transactions/index.ts
@@ -1,26 +1,9 @@
-export { isMsgRegisterDomain, MsgRegisterDomainStore } from "./RegisterDomain";
-export type { MsgRegisterDomain } from "./RegisterDomain";
-
-export { isMsgRenewDomain, MsgRenewDomainStore } from "./RenewDomain";
-export type { MsgRenewDomain } from "./RenewDomain";
-
-export { isMsgTransferDomainAll, MsgTransferDomainAllStore } from "./TransferDomain";
-export type { MsgTransferDomainAll } from "./TransferDomain";
-
-export { isMsgTransferAccount, MsgTransferAccountStore } from "./TransferAccount";
-export type { MsgTransferAccount } from "./TransferAccount";
-
-export { isMsgRegisterAccount, MsgRegisterAccountStore } from "./RegisterAccount";
-export type { MsgRegisterAccount } from "./RegisterAccount";
-
-export { isMsgDeleteDomain, MsgDeleteDomainStore } from "./DeleteDomain";
-export type { MsgDeleteDomain } from "./DeleteDomain";
-
-export { isMsgSetAccountMetadata } from "./SetAccountMetadata";
-export type { MsgSetAccountMetadata } from "./SetAccountMetadata";
-
-export { isMsgReplaceAccountResources } from "./ReplaceAccountResources";
-export type { MsgReplaceAccountResources } from "./ReplaceAccountResources";
-
-export { isMsgRenewAccount } from "./RenewAccount";
-export type { MsgRenewAccount } from "./RenewAccount";
+export * from "./RegisterDomain";
+export * from "./RenewDomain";
+export * from "./TransferDomain";
+export * from "./TransferAccount";
+export * from "./RegisterAccount";
+export * from "./DeleteDomain";
+export * from "./SetAccountMetadata";
+export * from "./ReplaceAccountResources";
+export * from "./RenewAccount";
