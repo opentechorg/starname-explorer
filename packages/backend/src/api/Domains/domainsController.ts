54d850fdc899acf9d64b547157e83177fded4115
Albert Andrejev <albert.andrejev@gmail.com>
2020-12-12 00:24:00 +0200

    Refactor domain controller

--- packages/backend/src/api/Domains/domainsController.ts
+++ packages/backend/src/api/Domains/domainsController.ts
@@ -1,33 +1,27 @@
-import { DomainSchemaModel } from "@starname-explorer/shared";
+import { Domain, DomainSchemaModel } from "@starname-explorer/shared";
 import { DefinedError } from "ajv";
 import { Request, Response } from "express";
+import { MongooseFilterQuery } from "mongoose";
 
-import validateDomainPageReq from "../../validators/domainPageRequest";
+import validateDomainPageReq, { Columns, DomainTablePageReq } from "../../validators/domainPageRequest";
 
 export class DomainsController {
   getDomains(req: Request, res: Response): void {
     if (!validateDomainPageReq(req.query)) {
-      for (const err of validateDomainPageReq.errors as DefinedError[]) {
-        console.error(err);
-      }
-      res.status(400).send(JSON.stringify(validateDomainPageReq.errors));
+      DomainsController.sendValidationError(res);
       return;
     }
 
-    let query = {};
-    if (req.query.query) {
-      const clearQuery = req.query.query.toLowerCase().replace(/[^a-z^0-9^\-^_]/g, "");
-      query = {
-        $or: [{ domain: new RegExp(clearQuery) }, { admin: new RegExp(clearQuery) }],
-      };
-    }
+    DomainsController.sendQueryResult(res, req.query);
+  }
 
+  private static sendQueryResult(res: Response, query: DomainTablePageReq): void {
     try {
-      DomainSchemaModel.findWithPages(query, {
-        page: req.query.page,
-        limit: req.query.limit,
+      DomainSchemaModel.findWithPages(DomainsController.getSearchQuery(query), {
+        page: query.page,
+        limit: query.limit,
         populate: "starnames",
-        sort: { [req.query.sortColumn]: req.query.sortOrder },
+        sort: DomainsController.getSortOrder(query),
       }).then(
         (page) => res.json(page),
         (reason) => console.error(reason),
@@ -35,5 +29,31 @@ export class DomainsController {
     } catch (err) {
       console.error(err);
     }
+  }
+
+  private static sendValidationError(res: Response): void {
+    for (const err of validateDomainPageReq.errors as DefinedError[]) {
+      console.error(err);
+    }
+    res.status(400).send(JSON.stringify(validateDomainPageReq.errors));
+  }
+
+  private static getSearchQuery(query: DomainTablePageReq): MongooseFilterQuery<Domain> {
+    if (query.query) {
+      const clearQuery = query.query.toLowerCase().replace(/[^a-z^0-9^\-^_]/g, "");
+      const regExpQuery = new RegExp(clearQuery);
+      return {
+        $or: [{ domain: regExpQuery }, { admin: regExpQuery }],
+      };
+    }
+
+    return {};
+  }
+
+  private static getSortOrder(query: DomainTablePageReq): Partial<{ [K in Columns]: number }> {
+    return {
+      [query.sortColumn]: query.sortOrder,
+      domain: query.sortColumn === "domain" ? query.sortOrder : 1,
+    };
   }
 }
