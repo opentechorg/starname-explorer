3f8f75004e2058ebdf9f5e48169a160d601f4232
Albert Andrejev <albert.andrejev@gmail.com>
2020-12-22 01:52:43 +0200

    Add redux

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -2,20 +2,26 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/styles";
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./routes";
+import { configureStore } from "./store";
 import theme from "./utils/theme";
+
+const store = configureStore();
 
 const render = (Component: React.ComponentType): void => {
   ReactDOM.render(
     <React.StrictMode>
-      <ThemeProvider theme={theme}>
-        <Router>
-          <CssBaseline />
-          <Component />
-        </Router>
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <Router>
+            <CssBaseline />
+            <Component />
+          </Router>
+        </ThemeProvider>
+      </Provider>
     </React.StrictMode>,
     document.getElementById("root"),
   );
