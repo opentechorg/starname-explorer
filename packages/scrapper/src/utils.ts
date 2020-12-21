201a19cc443d80027326d343fd0821a06859485d
Albert Andrejev <albert.andrejev@gmail.com>
2020-12-22 01:55:39 +0200

    Remove not used fles

--- packages/scrapper/src/utils.ts
+++
@@ -1,8 +0,0 @@
-import { LcdClient } from "@cosmjs/launchpad";
-
-import { setupStarnameExtension } from "./starname";
-import { LcdStarnameClient } from "./types";
-
-export function getStarnameClient(apiUrl: string): LcdStarnameClient {
-  return LcdClient.withExtensions({ apiUrl }, setupStarnameExtension);
-}
